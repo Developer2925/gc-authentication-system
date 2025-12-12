d0c4dbb0f7a9104fc9a1b16b5bd6eaac9acefcce
Om Dhakulkar <136986723+Developer2925@users.noreply.github.com>
2025-12-12 23:29:48 +0900

    Update backend URL for authentication

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
     password: "",
   });
   const navigate = useNavigate();
-  const backendURL = "http://localhost:3000";
+  const backendURL = "https://gc-authentication-system-server.onrender.com" //"http://localhost:3000";
 
   // Changing the values
   const handleChanges = (e) => {

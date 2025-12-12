595c4d9e499fb5ac0344a45a3f0b116f9000264f
Om Dhakulkar <136986723+Developer2925@users.noreply.github.com>
2025-12-12 23:41:49 +0900

    Fix import path for Register component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
